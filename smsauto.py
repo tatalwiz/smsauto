01ad389f634242930cc1f29208088e7a53ef4292
Kite-Kate <65412625+Kite-Kate@users.noreply.github.com>
2020-08-04 14:00:51 +0200

    Update smsauto.py

--- smsauto.py
+++ smsauto.py
@@ -9,13 +9,57 @@ from progress.bar import IncrementalBar
 import time
 
 
+filepath = filedialog.askopenfilename(filetypes=[("Ficher Excel", ".xlsx .xls")])
+columns = "AZERTYUIOPQSDFGHJKLMWXCVBN"
+length = 1000
+
+workbook = load_workbook(filename=filepath, read_only=True)
+worksheet = workbook.active
+
+phone_numbers = []
+
+bar = IncrementalBar("Analyse du fichier Excel en cours...", max=26000)
+
+for column in columns:
+	for i in range(length):
+		bar.next()
+		cell = "{}{}".format(column, i+1)
+		number = worksheet[cell].value
+		number = str(number)
+		delchar=[" ","/"]
+		
+		for char in delchar:
+			number = number.replace(char, "")
+		try:
+			number = int(number)
+		except:
+			a=1	
+		finally:
+			if str(type(number)) == "<class 'int'>":
+				number = str(number)
+				if len(number) == 9:
+					number = "0" + number
+					phone_numbers.append(number)
+				elif len(number) == 10:
+					phone_numbers.append(number)
+				elif number == "None":
+					t = 1
+				else:
+					print (f"{number} n'est pas un numéro valide")
+		
+bar.finish()
+time.sleep(2)
+phone_numbers = list( dict.fromkeys(phone_numbers) )
+print(f"\n{phone_numbers}")
+
+
 ip = simpledialog.askstring(title="IP de connexion", prompt="Merci de renseigner l'adresse IP du téléphone (voir application)")
 
 if ip == None:
 	messagebox.showwarning(title="Annulation", message="Envois annulés")
 	exit()
 
-ip = IPv4Address("ip")
+ip = IPv4Address(ip)
 session = AirmoreSession(ip)
 
 
@@ -25,50 +69,6 @@ if session.is_server_running:
 	if was_accepted:
 		print("Connexion autorisée")
 		service = MessagingService(session)
-		filepath = filedialog.askopenfilename(filetypes=[("Ficher Excel", ".xlsx .xls")])
-		columns = "AZERTYUIOPQSDFGHJKLMWXCVBN"
-		length = 1000
-
-		workbook = load_workbook(filename=filepath, read_only=True)
-		worksheet = workbook.active
-
-		phone_numbers = []
-
-		bar = IncrementalBar("Analyse du fichier Excel en cours...", max=26000)
-
-
-		for column in columns:
-			for i in range(length):
-				bar.next()
-				cell = "{}{}".format(column, i+1)
-				number = worksheet[cell].value
-				number = str(number)
-				delchar=[" ","/"]
-		
-				for char in delchar:
-					number = number.replace(char, "")
-				try:
-					number = int(number)
-				except:
-					a=1	
-				finally:
-		
-					if str(type(number)) == "<class 'int'>":
-						number = str(number)
-						if len(number) == 9:
-							number = "0" + number
-							phone_numbers.append(number)
-						elif len(number) == 10:
-							phone_numbers.append(number)
-						elif number == "None":
-							t = 1
-						else:
-							print (f"{number} n'est pas un numéro valide")
-
-		bar.finish()
-		time.sleep(2)
-		phone_numbers = list( dict.fromkeys(phone_numbers) )
-		print(f"\n{phone_numbers}")
 
 		valid = 0
 		while valid == 0:
